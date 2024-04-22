4a158332360b32ed17278faf7caa9ecbb71ac3f8
konPlantPG <hatasagoki@gmail.com>
2024-04-22 17:20:39 +0900

    publish

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'], // ページコンポーネントとして認識する拡張子を指定
 };
 
 export default nextConfig;
